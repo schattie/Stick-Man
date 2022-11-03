1c030d4da5fa86e49241a1e7ea02db21bc507aba
schattie <schattie.hunter@gmail.com>
2022-11-03 15:00:53 -0400

    added filed

---
+++ backend/controllers/userController.js
@@ -0,0 +1,20 @@
+const User = require('../models/userModel')
+
+//login user
+const loginUser = async (req, res) => {
+    res.json({mssg: 'login user'})
+}
+
+//register user
+const registerUser = async (req, res) => {
+    const {username, password} = req.body
+    try{
+        const user = await User.register(username, password) 
+
+        res.status(200).json({username, user})
+    }catch(error){
+        res.status(400).json({error: error.message})
+    }
+}
+
+module.exports = { registerUser, loginUser }
